7a87e7c1ce853b67d328536198049403cbe13f21
Rafael <rafaironm2@gmail.com>
2021-05-17 23:06:47 -0300

    OsVingadores_dicio

---
+++ OsVingadores_dicio.py
@@ -0,0 +1,14 @@
+'''lista_telefonica = [('Rafael', '11-9-7072.3887'), ('Fernanda', '11-9999.000'), ("Gustavo", '11-8888.0000'), ('Vicente', '11-7777.8888'), ('Luzia', '1111.2222')]
+print (lista_telefonica)
+print (lista_telefonica[0])
+print (lista_telefonica[0][0])
+
+contatos = dict (lista_telefonica)
+
+nome = input("Digite um nome: ")
+print (contatos.get(nome, "O nome não foi encontrado!"))'''
+
+vingadores = {'Chris Evans':'Capitão America', 'Mark Ruffalo':'Hulk', 'Tom Hiddleston': 'Loki', 'Chris Hemworth': 'Thor', 'Robert Downey Jr': 'Homem de Ferro', 'Scarlett Johansson':' Viúva Negra'}
+
+personagens = input ("Digite o nome de um personagem: ")
+print (vingadores.get (personagens, "O nome não foi encontrado!"))
